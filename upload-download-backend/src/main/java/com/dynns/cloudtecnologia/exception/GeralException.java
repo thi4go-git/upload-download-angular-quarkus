bf1a8086334a517dca41ac3ef9212b90dfdf77ae
thi4go-git <thi4go19@gmail.com>
2023-10-14 19:54:28 -0300

    Retirada Arquivos BUG

--- upload-download-backend/src/main/java/com/dynns/cloudtecnologia/exception/GeralException.java
+++
@@ -1,7 +0,0 @@
-package com.dynns.cloudtecnologia.exception;
-
-public class GeralException extends RuntimeException {
-    public GeralException(final String mensagem) {
-        super(mensagem);
-    }
-}
